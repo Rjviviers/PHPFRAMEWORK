8743b2124f128a38c31e4e32d4ce9f11ac0f9c7d
Ruan Viviers <rjviviers@gmail.com>
2022-09-08 13:37:08 +0200

    Cnew routing

--- Core/Router.php
+++ Core/Router.php
@@ -31,16 +31,33 @@ class Router{
     public function __construct(Request $request) {
         $this->request = $request;
     }
-    public function get(string $path = null, callable $callback = null){
-        $this->routes['Get'][$path] = $callback;
+    public function get( $path , $callback){
+        $this->routes['get'][$path] = $callback;
     }
 
     public function resolve()
     {
-        $this->request->getPath();   
+        $path = $this->request->getPath();  
+        $method = $this->request->getMethod(); 
+        $callback = $this->routes[$method][$path] ?? false;
+        if($callback === false){
+            return "not Found";
+        }
+        if (is_string($callback)){
+            return $this->renderView($callback);
+            
+        }
+            
+        return call_user_func($callback);
     }
    
-
+    public function renderView($view)
+    {
+        
+        include_once __DIR__ . "/../Views/$view.php";
+        
+    }
+   
 
 }
 
